1ea30249e9ee23ce4eb4ff1cd3493869fc092f04
m1 <222222@gmail.com>
2019-06-28 10:29:08 +0800

    [add] commit all progect

---
+++ src/main/java/com/imooc/repository/HousePictureRepository.java
@@ -0,0 +1,14 @@
+package com.imooc.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.imooc.entity.HousePicture;
+
+/**
+ * Created by 李肇忠.
+ */
+public interface HousePictureRepository extends CrudRepository<HousePicture, Long> {
+    List<HousePicture> findAllByHouseId(Long id);
+}
